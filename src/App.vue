1b2e01f6879a50524fadaf5c3138ab566adff45c
joj59 <jojijoseph@hotmail.co.uk>
2024-06-21 17:33:36 +0100

    project finished

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Ninja reaction timer</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+
+    <!-- <p v-if="showResults">reaction time - {{ score }}ms</p> -->
+
+    <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components: { Block, Results },
+    data() {
+        return {
+            isPlaying: false,
+            delay: 0,
+            score: 0,
+            showResults: false,
+        };
+    },
+    methods: {
+        start() {
+            this.delay = 2000 + Math.random() * 1000;
+            this.isPlaying = true;
+            this.showResults = false;
+        },
+        endGame(reactionTime) {
+            this.score = reactionTime;
+            this.isPlaying = false;
+            this.showResults = true;
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,48 @@
+<template>
+    <h1>You reacted in {{ score }}ms</h1>
+    <h5>Rank: {{ rank }}</h5>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        };
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'ninja fingers';
+        } else if (this.score < 400) {
+            this.rank = 'rapid';
+        } else {
+            this.rank = 'snail pace';
+        }
+    },
+};
+</script>
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+
+button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+}
+button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+}
+</style>
